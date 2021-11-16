14a5f8ca5d8efa3cdce2c34a47f451a8595abec1
yedorye <dory.ye@whaple.com>
2021-11-16 12:02:22 +0900

    :tada:  Init a project
    Closes #1

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+const HomeWrapper = styled.div`
+  background: orange;
+`
+
+const Home = () => {
+  return <HomeWrapper>Hello</HomeWrapper>
+}
+
+export default Home
